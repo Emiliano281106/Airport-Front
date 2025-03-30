f27151dc89dc166aa29b24cb2ce09ab20b853d06
Emiliano <emilianomendozaliscano@gmail.com>
2025-03-30 17:40:47 +0200

    axios get all

--- src/components/Flights.jsx
+++ src/components/Flights.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from "@mui/material/Box";
+import axios from '../api';
+import Button from '@mui/material/Button';
 
 const Flights = () => {
+
+  const [flights, setFlights] = useState([]);
+
+  const getFlights = async () => {
+    try {
+      const response = await axios.get("/flights/getFlights");
+      console.log("API Response:", response);
+      setFlights(response.data || []); 
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getFlights();
+  }, []);
+
+  const deleteFlight = async (id) => {
+    try {
+      const response = await axios.delete(`/flights/${id}`);
+      console.log("API response:", response);
+      alert("Flight deleted successfully!");
+      getFlights();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Coming Flights</h1>
-      <p>This is the flights section.</p>
-    </div>
+    <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+      {flights.map((flight) => (
+        <Box
+          gridColumn={{ xs: "span 12", sm: "span 6", md: "span 3" }}
+          key={flight.id}
+        >
+          <Card>
+            <CardContent>
+              {flight.flightNumber}
+              Departure Time: {flight.departureTime}
+              Arrival Time: {flight.arrivalTime}
+              Status: {flight.status}
+            </CardContent>
+            <Button variant="outlined" onClick={() => { deleteFlight(flight.id) }}>
+              {" "}
+              delete{" "}
+            </Button>
+          </Card>
+        </Box>
+      ))}
+    </Box>
   );
 };
 
--- src/components/Planes.jsx
+++ src/components/Planes.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from "@mui/material/Box";
+import axios from '../api';
+import Button from '@mui/material/Button';
 
 const Planes = () => {
+
+  const [planes, setPlanes] = useState([]);
+
+  const getPlanes = async () => {
+    try {
+      const response = await axios.get("/planes/getPlanes");
+      console.log("API Response:", response);
+      setPlanes(response.data || []); 
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getPlanes();
+  }, []);
+
+  const deletePlane = async (id) => {
+    try {
+      const response = await axios.delete(`/planes/${id}`);
+      console.log("API response:", response);
+      alert("Plane deleted successfully!");
+      getPlanes();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>These Are Our Planes</h1>
-      <p>This is the planes section.</p>
-    </div>
+    <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+      {planes.map((plane) => (
+        <Box
+          gridColumn={{ xs: "span 12", sm: "span 6", md: "span 3" }}
+          key={plane.id}
+        >
+          <Card>
+            <CardContent>
+              {plane.model}
+              Manufacturer: {plane.manufacturer}
+              Capacity: {plane.capacity}
+            </CardContent>
+            <Button variant="outlined" onClick={() => { deletePlane(plane.id) }}>
+              {" "}
+              delete{" "}
+            </Button>
+          </Card>
+        </Box>
+      ))}
+    </Box>
   );
 };
 
